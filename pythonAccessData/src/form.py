362db8724ae3da6190b6489d8c5bb88ded6f0a90
AnsharTwo <robert.tomsett@btinternet.com>
2024-08-03 17:54:22 +0100

    more search funcs and validation added for all so far

--- pythonAccessData/src/form.py
+++ pythonAccessData/src/form.py
@@ -1,5 +1,6 @@
 import sys
 import streamlit as st
+from tornado import auth
 
 import db
 
@@ -37,17 +38,61 @@ class DATA_FORM:
             st.form_submit_button("Go")
             if searchSelection == self.dict_searches.get("ants_srch_txt"):
                 self.srch_searchtext()
+            elif searchSelection == self.dict_searches.get("ants_srch_txt_auth"):
+                self.srch_searchtext_auth()
+            elif searchSelection == self.dict_searches.get("ants_srch_txt_bk"):
+                self.srch_searchtext_bk()
+            elif searchSelection == self.dict_searches.get("ants_bk"):
+                self.srch_bk()
 
     def srch_searchtext(self):
         txt = st.text_area("Annotated text to search for (separate multiple with comma)")
         searched = st.form_submit_button("Search")
         if searched:
-            self.db_records(self.dict_searches.get("ants_srch_txt"), txt, "")
+            if txt == "":
+                st.markdown(":red[no search text given.]")
+            else:
+                self.db_records(self.dict_searches.get("ants_srch_txt"), txt, "", "")
+
+    def srch_searchtext_auth(self):
+        txt = st.text_area("Annotated text to search for (separate multiple with comma)")
+        author = st.text_input("Author")
+        searched = st.form_submit_button("Search")
+        if searched:
+            if txt == "":
+                st.markdown(":red[no search text given.]")
+            else:
+                if author == "":
+                    st.markdown(":red[no author given.]")
+                else:
+                    self.db_records(self.dict_searches.get("ants_srch_txt_auth"), txt, author, "")
+
+    def srch_searchtext_bk(self):
+        txt = st.text_area("Annotated text to search for (separate multiple with comma)")
+        book = st.text_input("Book")
+        searched = st.form_submit_button("Search")
+        if searched:
+            if txt == "":
+                st.markdown(":red[no search text given.]")
+            else:
+                if book == "":
+                    st.markdown(":red[no book given.]")
+                else:
+                    self.db_records(self.dict_searches.get("ants_srch_txt_bk"), txt, "", book)
+
+    def srch_bk(self):
+        book = st.text_input("Book")
+        searched = st.form_submit_button("Search")
+        if searched:
+            if book == "":
+                st.markdown(":red[no book given.]")
+            else:
+                self.db_records(self.dict_searches.get("ants_bk"), "", "", book)
 
     def select_url_search(self):
         st.write("Page is pending, under construction")
 
-    def db_records(self, searchSelection, searchText, auth):
+    def db_records(self, searchSelection, searchText, auth, bk):
         dbPath = sys.argv[1] + sys.argv[2]
 
         sourceData = db.DATA_SOURCE(r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=%s;' % dbPath)
@@ -64,18 +109,36 @@ class DATA_FORM:
             for ant in annots:
                 st.write(
                     f"{ant.__getattribute__('Book Title')}\t{ant.Author}\t{ant.__getattribute__('Book No')}\t{ant.__getattribute__('Page No')}\t{ant.__getattribute__('Source Text')}")
+
         elif searchSelection == self.dict_searches.get("ants_srch_txt_auth"):
             resCountSrchStrAndAuthor = sourceData.resAnnotsbySrchStrAndAuthor(conn.cursor(), searchText, auth)
             annots = sourceData.selectAnnotsbySrchStrAndAuthor(conn.cursor(),  searchText, auth)
-
             st.write("Found {} results.".format(resCountSrchStrAndAuthor))
             for ant in annots:
                 st.write(
                     f"{ant.Author}\t{ant.__getattribute__('Book Title')}\t{ant.__getattribute__('Book No')}\t{ant.__getattribute__('Page No')}\t{ant.__getattribute__('Source Text')}")
 
+        elif searchSelection == self.dict_searches.get("ants_srch_txt_bk"):
+            resCountSrchStrAndBook = sourceData.resAnnotsbySrchStrAndBook(conn.cursor(), searchText, bk)
+            annots = sourceData.selectAnnotsbySrchStrAndBook(conn.cursor(), searchText, bk)
+            st.write("Found {} results.".format(resCountSrchStrAndBook))
+            for ant in annots:
+                st.write(f"{ant.__getattribute__('Book Title')}\t{ant.Author}\t{ant.__getattribute__('Book No')}\t{ant.__getattribute__('Page No')}\t{ant.__getattribute__('Source Text')}")
+
+        elif searchSelection == self.dict_searches.get("ants_bk"):
+            # use \'' if single quote is in e.g. hitler's
+            resCountBooks = sourceData.resAnnotsbyBook(conn.cursor(), bk)
+            annots = sourceData.selectAnnotsbyBook(conn.cursor(), bk)
+            st.write("Found {} results.".format(resCountBooks))
+            st.write(bk)
+            for ant in annots:
+                st.write(f"{ant.__getattribute__('Book No')}\t{ant.__getattribute__('Page No')}\t{ant.__getattribute__('Source Text')}")
+
         conn.close()
 
     dict_searches = {
         "ants_srch_txt": "Annotations by search text",
-        "ants_srch_txt_auth": "Annotations by search text and author"
+        "ants_srch_txt_auth": "Annotations by search text and author",
+        "ants_srch_txt_bk": "Annotations by search text and book",
+        "ants_bk": "Annotations by book"
     }
