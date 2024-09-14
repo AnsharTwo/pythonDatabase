25776b19b4b640e2fc25685c72da1c59439b7200
AnsharTwo <robert.tomsett@btinternet.com>
2024-09-14 19:00:43 +0100

    wip

--- pythonAccessData/src/sidebar.py
+++ pythonAccessData/src/sidebar.py
@@ -27,18 +27,18 @@ class SIDEBAR:
 
         st.sidebar.title("Annotations and URL manager")
         dropSelectApp = st.sidebar.selectbox("Select to view", [self.dict_data_app.get("annotDb"),
-                                                    self.dict_data_app.get("urlExcel")])
+                                                    self.dict_data_app.get("urlExcel"), "None"],
+                                            )
         if dropSelectApp == self.dict_data_app.get("annotDb"):
             dataForm.select_search()
         elif dropSelectApp == self.dict_data_app.get("urlExcel"):
             sheetForm.select_url_search()
 
-        st.sidebar.title("Edit data manager")
-        dropSelectApp = st.sidebar.selectbox("Select to do", ["None",
+        dropSelectDataApp = st.sidebar.selectbox("Select to do", ["None",
                                                               self.dict_edit_data_app.get("annotDb"),
-                                                              self.dict_edit_data_app.get("urlExcel")])
-        if dropSelectApp == self.dict_edit_data_app.get("annotDb"):
+                                                              self.dict_edit_data_app.get("urlExcel")]
+                                                 )
+        if dropSelectDataApp == self.dict_edit_data_app.get("annotDb"):
             editForm.select_edit_form()
-        elif dropSelectApp == self.dict_edit_data_app.get("urlExcel"):
+        elif dropSelectDataApp == self.dict_edit_data_app.get("urlExcel"):
             editSheetForm.select_edit_form_sheet()
-
