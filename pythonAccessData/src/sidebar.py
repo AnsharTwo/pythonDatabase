ba712e4486cfb07bf751fa403325525c46ed7286
AnsharTwo <robert.tomsett@btinternet.com>
2024-09-16 20:23:55 +0100

    for PR

--- pythonAccessData/src/sidebar.py
+++ pythonAccessData/src/sidebar.py
@@ -18,7 +18,15 @@ class SIDEBAR:
         "annotDb": "Annotations database",
         "urlExcel": "Excel URLs sheets"
     }
+
+    dict_tabh_drs = {
+        "Read": "View",
+        "Edit": "Do"
+    }
+
     def init_sidebars(self):
+
+        tabViewData, tabEditData = st.tabs([self.dict_tabh_drs.get("Read"), self.dict_tabh_drs.get("Edit")])
 
         dataForm = form.DATA_FORM()
         sheetForm = form_sheet.SHEET_FORM()
@@ -26,19 +34,24 @@ class SIDEBAR:
         editSheetForm = form_sheet_edit.EDIT_SHEET_FORM()
 
         st.sidebar.title("Annotations and URL manager")
+        st.session_state.disabled = False
+
         dropSelectApp = st.sidebar.selectbox("Select to view", [self.dict_data_app.get("annotDb"),
-                                                    self.dict_data_app.get("urlExcel"), "None"],
+                                                                self.dict_data_app.get("urlExcel"), "None"]
                                             )
         if dropSelectApp == self.dict_data_app.get("annotDb"):
-            dataForm.select_search()
+            with tabViewData:
+                dataForm.select_search()
         elif dropSelectApp == self.dict_data_app.get("urlExcel"):
-            sheetForm.select_url_search()
+            with tabViewData:
+                sheetForm.select_url_search()
 
-        dropSelectDataApp = st.sidebar.selectbox("Select to do", ["None",
-                                                              self.dict_edit_data_app.get("annotDb"),
-                                                              self.dict_edit_data_app.get("urlExcel")]
-                                                 )
+        dropSelectDataApp = st.sidebar.selectbox("Select to do", [self.dict_edit_data_app.get("annotDb"),
+                                                                  self.dict_edit_data_app.get("urlExcel"),"None"],
+                                                )
         if dropSelectDataApp == self.dict_edit_data_app.get("annotDb"):
-            editForm.select_edit_form()
+            with tabEditData:
+                editForm.select_edit_form()
         elif dropSelectDataApp == self.dict_edit_data_app.get("urlExcel"):
-            editSheetForm.select_edit_form_sheet()
+            with tabEditData:
+                editSheetForm.select_edit_form_sheet()
