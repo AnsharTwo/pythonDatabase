2f3f92c7ba553b708f5ccad8c8e74bd75bfd1a21
AnsharTwo <robert.tomsett@btinternet.com>
2024-10-07 18:38:28 +0100

    added new alg for 3 frm, 2-stage forms alg not worked

--- pythonAccessData/src/form_edit.py
+++ pythonAccessData/src/form_edit.py
@@ -36,7 +36,8 @@ class EDIT_FORM:
             self.dict_edit_annot_sel.get("ants_edt_add"),
             self.dict_edit_annot_sel.get("ants_edt_edt"),
             self.dict_edit_annot_sel.get("ants_edt_dlt"),
-            self.dict_edit_annot_sel.get("ants_add_bk")
+            self.dict_edit_annot_sel.get("ants_add_bk"),
+            "t"
         ])
         if editSelection == self.dict_edit_annot_sel.get("ants_edt_add"):
             self.edt_new_annot()
@@ -46,6 +47,43 @@ class EDIT_FORM:
             self.edt_dlt_annot()
         elif editSelection == self.dict_edit_annot_sel.get("ants_add_bk"):
             self.add_new_bk()
+        elif editSelection == "t":
+            self.test()
+
+    def test(self):
+
+        print("got here")
+        def generate_storyline():
+            st.session_state["stage"] = "edit_storyline"
+
+        def create_booklet():
+            st.session_state["stage"] = "final_parameters"
+
+        def final_parameters():
+            st.session_state["stage"] = "story_generation"
+
+        if "stage" not in st.session_state:
+            st.session_state["stage"] = "story_generation"
+
+        if st.session_state["stage"] == "story_generation":
+            with st.form("story_form"):
+                submitted = st.form_submit_button(
+                    "Generate Storyline", on_click=generate_storyline)
+                submittedB = st.form_submit_button(
+                    "Generate StorylineB", on_click=create_booklet
+
+                )
+
+        elif st.session_state["stage"] == "edit_storyline":
+            with st.form("edit_story_form"):
+                submitted = st.form_submit_button("Create Booklet", on_click=create_booklet)
+
+        elif st.session_state["stage"] == "final_parameters":
+            with st.form("final_params_form"):
+                submitted = st.form_submit_button("Create Book", on_click=final_parameters)
+
+
+
 
     def edt_new_annot(self):
         placeholder = st.empty()
