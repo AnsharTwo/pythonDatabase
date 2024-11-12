8215871a57a6da5fdf08050fd82819105512b0f6
AnsharTwo <robert.tomsett@btinternet.com>
2024-11-12 18:53:12 +0000

    mutli-words with .. working spell check

--- pythonAccessData/src/form_edit.py
+++ pythonAccessData/src/form_edit.py
@@ -300,45 +300,6 @@ class EDIT_FORM:
                 if st.session_state["spell_txt_area"] == "":
                     st.session_state["spell_txt_area"] =  st.session_state["annot_txt_area"]
                     spell_check_list = st.session_state["spell_txt_area"].split()
-
-                    #########################################################
-                    # TODO - wright p56 "..open..party." disregard quotes - this si not setting to split words true as the first .. are found.
-                    # split_wrds = False
-                    # for ctr in range(0, len(spell_check_list)):
-                    #     if split_wrds:
-                    #         #spell_check_list.pop(ctr - 1)
-                    #         split_wrds = False
-                    #     if str(spell_check_list[ctr]).find("..") != -1:
-                    #         tmp_wrd = str(spell_check_list[ctr])
-                    #         if tmp_wrd.startswith(".."):
-                    #             tmp_wrd = tmp_wrd.lstrip("..")
-                    #             split_wrds = True
-                    #         if tmp_wrd.endswith(".."):
-                    #             tmp_wrd = tmp_wrd.rstrip("..")
-                    #             if not split_wrds:
-                    #                 split_wrds = True
-                    #         while tmp_wrd.count("..") > 0:
-                    #             split_wrds = True
-                    #             tmp_wrds = tmp_wrd.split("..")
-                    #             spell_check_list.append(tmp_wrds[0])
-                    #             tmp_wrd = tmp_wrd.lstrip(tmp_wrd + "..")
-                    #         if split_wrds:
-                    #             for w in range(1, len(tmp_wrds)):
-                    #                 spell_check_list.append(str(tmp_wrds[w]))
-                    #             spell_check_list.pop(ctr)
-                    #         tmp_wrds.clear()
-                    #
-                    # for w in spell_check_list:
-                    #     st.write(str(w))
-                    # st.write("................................")
-
-
-                    #for ctr in range(0, len(spell_check_list)):
-                    #    st.write("word in spell list: " + str(ctr) + " " + str(spell_check_list[ctr]))
-                    #    # debug code ###############################
-
-                    #########################################################
-
                     self.__format_spell_List_words(spell_check_list)
                     temp_wrds_unknwn = spell.unknown(spell_check_list)
                     temp_spell_list = []
@@ -453,8 +414,8 @@ class EDIT_FORM:
     def __format_spell_List_words(self, spell_check_list):
 
         # TODO here.
-        # 1 break up words like word1..wrd2 split ..
-        # 2 - test string in xls - repeats highlight of spell word inside another word - fix
+        # 1 - test string in xls - repeats highlight of spell word inside another word - fix
+        # handle " adding for start adn end of annot
 
         split_wrds = False
         pref_postf_remd = False
@@ -464,54 +425,37 @@ class EDIT_FORM:
                 split_wrds = False
             if pref_postf_remd:
                 pref_postf_remd = False
-
-            # TO DO This is not finding all words for the one case use "The.. ..test..was for this and..that." - ..test..was is the problem
-            # is it ctr after pop index wrong?
             if str(spell_check_list[ctr]).find("..") != -1: # special case to handle all pos. instances of ..
                 tmp_wrd = str(spell_check_list[ctr])
-
-                st.write("Got here with " + tmp_wrd)
-
                 if tmp_wrd.startswith(".."):
                     tmp_wrd = tmp_wrd.lstrip("..")
-
-
                     pref_postf_remd = True
                 if tmp_wrd.endswith(".."):
                     tmp_wrd = tmp_wrd.rstrip("..")
-
-                    #st.write("Got here with " + tmp_wrd)
-
                     if not pref_postf_remd:
                         pref_postf_remd = True
                 if tmp_wrd.count("..") > 0:
+                    splt_wrds_indx = 0
+                    wrds_to_add_spll_lst = []
                     while tmp_wrd.count("..") > 0:
                         split_wrds = True
                         tmp_wrds = tmp_wrd.split("..")
-                        spell_check_list.append(tmp_wrds[0])
-                        tmp_wrd = tmp_wrd.lstrip(tmp_wrd + "..")
+                        wrds_to_add_spll_lst.insert(splt_wrds_indx, tmp_wrds[0])
+                        tmp_wrd = tmp_wrd.lstrip(str(tmp_wrds[0]) + "..")
+                        splt_wrds_indx += 1
+                    wrds_to_add_spll_lst.insert(splt_wrds_indx, tmp_wrds[1])
                     if split_wrds:
-                        for w in range(1, len(tmp_wrds)):
-                            spell_check_list.append(str(tmp_wrds[w]))
                         spell_check_list.pop(ctr)
-
-
+                        spell_check_list.insert(ctr, str(wrds_to_add_spll_lst[0]))
+                        w = 1
+                        for w in range(1, len(wrds_to_add_spll_lst)):
+                            spell_check_list.append(str(wrds_to_add_spll_lst[w]))
+                        wrds_to_add_spll_lst.clear()
                     tmp_wrds.clear()
                 else:
                     if pref_postf_remd:
-
-                        #st.write("Got here with " + tmp_wrd)
-                        spell_check_list.append(tmp_wrd)#
-
-
                         spell_check_list.pop(ctr)
-                        pref_postf_remd = False
-
-
-        for w in spell_check_list:
-            st.write(str(w))
-        st.write("................................")
-
+                        spell_check_list.insert(ctr, tmp_wrd)
         trim_chars_list = ["\"'", "'\"", "'.", ".'"] # works - add more multi-char if not caught by single char list below
         for chr in trim_chars_list:
             for sp_ctr in range(0, len(spell_check_list)):
@@ -522,7 +466,6 @@ class EDIT_FORM:
         trim_char_list = ['"', """'""", "", ".", ",", ";", ":", "(", ")", "[", "]", "{", "}", "<", ">", "?", "!", "£", "$"]
         for chr in trim_char_list:
             for sp_ctr in range(0, len(spell_check_list)):
-                #if str(spell_check_list[sp_ctr]).find(str(chr), 0, 0) != -1:
                 if str(spell_check_list[sp_ctr]).find(str(chr)) == 0:
                     temp_char = str(spell_check_list[sp_ctr])
                     spell_check_list.pop(sp_ctr)
