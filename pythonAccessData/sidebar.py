5a4fc0582330089b3259a939220ff061e7e43ec5
AnsharTwo <robert.tomsett@btinternet.com>
2025-03-04 20:59:11 +0000

    sidebar bttons and settings tab

--- pythonAccessData/sidebar.py
+++ pythonAccessData/sidebar.py
@@ -1,10 +1,7 @@
 import streamlit as st
+from streamlit.components.v1 import html
 import form_sr
-import form_view
-import form_sheet
-import form_bk
-import form_annt
-import form_sheet_edit
+import form_home
 
 class SIDEBAR (form_sr.FORM):
 
@@ -24,69 +21,66 @@ class SIDEBAR (form_sr.FORM):
 
     dict_tabh_drs = {
         "Read": "View",
-        "Edit": "Do"
+        "Edit": "Do",
+        "Config": "Settings"
     }
 
     def init_sidebars(self):
-        db_viewer = form_view.DATA_FORM()
-        annt_worker_form = form_annt.EDIT_ANNOT()
-        bk_worker_form = form_bk.EDIT_BOOK()
-        sheetForm = form_sheet.SHEET_FORM()
-        editSheetForm = form_sheet_edit.EDIT_SHEET_FORM()
-        tabViewData, tabEditData = st.tabs([self.dict_tabh_drs.get("Read"), self.dict_tabh_drs.get("Edit")])
+        home = form_home.HOME_FORM()
+        tabViewData, tabEditData, tabSettingsData = st.tabs([self.dict_tabh_drs.get("Read"),
+                                                             self.dict_tabh_drs.get("Edit"),
+                                                             self.dict_tabh_drs.get("Config")])
         st.sidebar.write(":green[Welcome, {}!]".format(self.user))
-        dropSelectApp = st.sidebar.selectbox("Select to view", [self.dict_data_app.get("annotDb"),
+        dropSelectApp = st.sidebar.selectbox("Select what to view", [self.dict_data_app.get("annotDb"),
                                                                 self.dict_data_app.get("urlExcel"), "None"])
         if dropSelectApp == self.dict_data_app.get("annotDb"):
             with tabViewData:
-                itm_slctd = self.select_edit_form(db_viewer.dict_List_view.get("header"),
-                                                        db_viewer.dict_List_view.get("title"),
-                                                        db_viewer.dict_searches)
-                if itm_slctd == db_viewer.dict_searches.get("ants_srch_txt"):
-                    db_viewer.srch_searchtext()
-                elif itm_slctd == db_viewer.dict_searches.get("ants_srch_txt_auth"):
-                    db_viewer.srch_searchtext_auth()
-                elif itm_slctd == db_viewer.dict_searches.get("ants_srch_txt_bk"):
-                    db_viewer.srch_searchtext_bk()
-                elif itm_slctd == db_viewer.dict_searches.get("ants_bk"):
-                    db_viewer.srch_bk()
-                elif itm_slctd == db_viewer.dict_searches.get("ants_auth"):
-                    db_viewer.srch_auth()
-                elif itm_slctd == db_viewer.dict_searches.get("bks_auth"):
-                    db_viewer.bks_auth()
-                elif itm_slctd == db_viewer.dict_searches.get("bks_all"):
-                    db_viewer.bks_all()
-                elif itm_slctd == db_viewer.dict_searches.get("bks_yr_read"):
-                    db_viewer.bks_yr_read()
-                elif itm_slctd == db_viewer.dict_searches.get("ants_all"):
-                    db_viewer.ants_all()
-                elif itm_slctd == db_viewer.dict_searches.get("ants_yr_read"):
-                    db_viewer.ants_yr_read()
+                home.select_view_ant()
         elif dropSelectApp == self.dict_data_app.get("urlExcel"):
             with tabViewData:
-                sheetForm.select_url_search()
-        dropSelectDataApp = st.sidebar.selectbox("Select to do", [self.dict_edit_data_app.get("annotDb"),
+                home.select_view_st()
+        dropSelectDataApp = st.sidebar.selectbox("Select what to do", [self.dict_edit_data_app.get("annotDb"),
                                                                   self.dict_edit_data_app.get("urlExcel"),"None"])
         if dropSelectDataApp == self.dict_edit_data_app.get("annotDb"):
             with (tabEditData):
-                itm_slctd = self.select_edit_form(self.dict_list_annt_wrkr.get("header"),
-                                                        self.dict_list_annt_wrkr.get("title"),
-                                                        self.dict_list_wrkr_items)
-                if itm_slctd == self.dict_list_wrkr_items.get("ants_edt_add"):
-                    annt_worker_form.edt_new_annot()
-                elif itm_slctd == self.dict_list_wrkr_items.get("bk_add_update_bk"):
-                    bk_worker_form.add_new_bk()
+                home.select_do_ant()
         elif dropSelectDataApp == self.dict_edit_data_app.get("urlExcel"):
             with tabEditData:
-                editSheetForm.select_edit_form_sheet()
+                home.select_do_st()
         st.sidebar.divider()
-        cl = st.sidebar.columns(2,gap="small", vertical_alignment="center")
-        cl[0].write("Settings")
-        #cl[1].link_button("", f"http://localhost:8501/Settings", icon=":material/settings:", type="secondary", use_container_width=True)
-
-        ########
-        cl[1].link_button("", f"http://localhost:8501/Settings?auth_stat={st.session_state.authentication_status}", icon=":material/settings:", type="secondary",
-                          use_container_width=True)
-        ########
-
+        btn_view = st.sidebar.button("View", use_container_width=True)
+        btn_do = st.sidebar.button("Do", use_container_width=True)
         st.sidebar.divider()
+        btn_sttngs = st.sidebar.button("Settings", icon=":material/settings:")
+        if btn_view:
+            self.button_tab_switch("View", "View")
+        if btn_do:
+            self.button_tab_switch("Do", "Do")
+        if btn_sttngs:
+            self.button_tab_switch("Settings", "Settings")
+        st.sidebar.divider()
+
+    def button_tab_switch(self, button_text, tab_text):
+        html(f"""<script>
+        (() => {{
+            let button = [...window.parent.document.querySelectorAll("button")].filter(button => {{
+                console.log(">>>", button.innerText)
+                return button.innerText.includes("{button_text}")
+            }})[0];
+            if(button) {{
+                button.onclick = () => {{
+                    var tabGroup = window.parent.document.getElementsByClassName("stTabs")[0]
+                    const tabButton = [...tabGroup.querySelectorAll("button")].filter(button => {{
+                        return button.innerText.includes("{tab_text}")
+                    }})[0];
+                    if(tabButton) {{
+                        tabButton.click();
+                    }} else {{
+                        console.log("tab button {tab_text} not found")
+                    }}
+                }}
+            }} else {{
+                console.log("button not found: {button_text}")
+            }}
+        }})();
+        </script>""", height=0)
