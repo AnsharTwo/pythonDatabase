2d4e44af48434c585e52ef569db8d67c7be7c485
AnsharTwo <robert.tomsett@btinternet.com>
2025-03-10 20:01:50 +0000

    wip 3

--- pythonAccessData/form_settings.py
+++ pythonAccessData/form_settings.py
@@ -10,7 +10,8 @@ class CONFIG_FORM (form_sr.FORM):
     dict_config = {
         "json_config": "config.json",
         "wdgt_specs": {
-            "inpt_ant_edt_hght": 3
+            "inpt_ant_edt_hght": 3,
+            "inpt_ant_edt_hght_minval": 68
         }
     }
 
@@ -35,13 +36,17 @@ class CONFIG_FORM (form_sr.FORM):
                 st.session_state.inpt_ant_edt_hght = cols_wrkspc_sz[0].text_input("Annotations editor height (pixels)",
                                                                                   value=inpt_ant_edt_hght,
                                                                                   max_chars=self.dict_config.get("wdgt_specs").get("inpt_ant_edt_hght"),
-                                                                                  help="must be at least 68 pixels")
+                                                                                  help="""must be at least """ +
+                                                                                       str(self.dict_config.get("wdgt_specs").get("inpt_ant_edt_hght_minval")) +
+                                                                                       """ pixels""")
                 st.divider()
                 cols_config = st.columns(2, gap="small", vertical_alignment="center")
                 if cols_config[0].form_submit_button("Save"):
                     can_save = True
-                    if st.session_state.inpt_ant_edt_hght == "" or not st.session_state.inpt_ant_edt_hght.isdigit():
-                        st.markdown(":red[Annotations editor height must entered as a number up to 3 digits.]")
+                    if st.session_state.inpt_ant_edt_hght == "" or not st.session_state.inpt_ant_edt_hght.isdigit() \
+                                                                or int (st.session_state.inpt_ant_edt_hght) < \
+                                                                    self.dict_config.get("wdgt_specs").get("inpt_ant_edt_hght_minval"):
+                        st.markdown(":red[Annotations editor height must entered as a number up to 3 digits, and not lesser than 68.]")
                         can_save = False
                     if can_save:
                         config_data['librotate_config']['widget_dims']['textarea_annot_height'] = st.session_state.inpt_ant_edt_hght
