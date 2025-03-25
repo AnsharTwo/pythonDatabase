62c2ec1c66e692ac79b2957dc87bebb2e4a4f80a
AnsharTwo <robert.tomsett@btinternet.com>
2025-03-25 20:35:14 +0000

    data editor wip 1

--- pythonAccessData/form_sheet_edit.py
+++ pythonAccessData/form_sheet_edit.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import pandas as pd
 import form_sr
 
 class EDIT_SHEET_FORM(form_sr.FORM):
@@ -19,6 +18,13 @@ class EDIT_SHEET_FORM(form_sr.FORM):
         "do_sites": "Create, edit or delete a website entry"
     }
 
+    dict_book_sheets_spec = {
+        "web_pages":
+            {
+                "url": "URL"
+            }
+    }
+
     def webpages_do_new_entry(self):
         st.session_state.do_webpages_form_flow = "do_webpages"
 
@@ -28,11 +34,14 @@ class EDIT_SHEET_FORM(form_sr.FORM):
         self.webpages_do_new_entry()
         if st.session_state.do_webpages_form_flow == "do_webpages":
             sheet_web_pages = self.load_book_sheet(self.dict_book_sheets.get("web_pages"))
-            st.write("Contents of 'pages':\n", sheet_web_pages)
+            edit_sheet_wbpgs = st.data_editor(sheet_web_pages, num_rows="dynamic",
+                                              column_config={
+                                                "URL": st.column_config.LinkColumn(self.dict_book_sheets_spec.get("web_pages").get("url"))
+                                              })
+            # TODO - edit_sheet_wbpgs write to file, use button in form likely bett than on_change AMMD USE small data xls
 
     def select_edt_sht_videos(self):
         sheet_videos = self.load_book_sheet(self.dict_book_sheets.get("videos"))
-        st.write("Contents of 'pages':\n", sheet_videos)
 
     def select_edt_sht_sites(self):
         sheet_sites = self.load_book_sheet(self.dict_book_sheets.get("sites"))
