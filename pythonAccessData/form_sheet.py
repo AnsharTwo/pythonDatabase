b8177e7d3642eefc568590bc6eea2563290bf520
AnsharTwo <robert.tomsett@btinternet.com>
2025-04-09 15:31:39 +0100

    search sum working

--- pythonAccessData/form_sheet.py
+++ pythonAccessData/form_sheet.py
@@ -110,6 +110,9 @@ class SHEET_FORM(form_sr.FORM):
                             df_srch = sqldf(self.dict_sql_df.get("web_pages").format(
                                                                 self.format_sql_wrap(st.session_state.webpages_st_srch_str)),
                                             env=None)
+                            row_sum = sqldf(self.dict_sql_df.get("web_pages_count").format(
+                                                                self.format_sql_wrap(st.session_state.webpages_st_srch_str)),
+                                            env=None)
                         else:
                             df_srch = sqldf(
                                 self.dict_sql_df.get("web_pages_and_url").format(self.format_sql_wrap(
@@ -117,16 +120,22 @@ class SHEET_FORM(form_sr.FORM):
                                                                                        self.format_sql_wrap(
                                                                             st.session_state.webpages_st_srch_str)),
                                 env=None)
-                        st.header("Sheet rows")
-                        st.dataframe(df_srch, hide_index=True,
-                                     column_order=(self.dict_book_sheets_spec.get("videos").get("desc"),
-                                                   self.dict_book_sheets_spec.get("videos").get("read"),
-                                                   self.dict_book_sheets_spec.get("videos").get("url"),
-                                                   self.dict_book_sheets_spec.get("videos").get("note")),
-                                     column_config={
-                                         "URL": st.column_config.LinkColumn(
-                                             self.dict_book_sheets_spec.get("videos").get("url")),
-                                     })
+                            row_sum = sqldf(self.dict_sql_df.get("web_pages_and_url_count").format(self.format_sql_wrap(
+                                                                            st.session_state.webpages_st_srch_str),
+                                                                                       self.format_sql_wrap(
+                                                                            st.session_state.webpages_st_srch_str)),
+                                env=None)
+                        if self.write_srch_row_sum(row_sum) != "0":
+                            st.header("Sheet rows")
+                            st.dataframe(df_srch, hide_index=True,
+                                         column_order=(self.dict_book_sheets_spec.get("videos").get("desc"),
+                                                       self.dict_book_sheets_spec.get("videos").get("read"),
+                                                       self.dict_book_sheets_spec.get("videos").get("url"),
+                                                       self.dict_book_sheets_spec.get("videos").get("note")),
+                                         column_config={
+                                             "URL": st.column_config.LinkColumn(
+                                                 self.dict_book_sheets_spec.get("videos").get("url")),
+                                         })
 
     def select_srch_videos(self):
         st.write("Under construction - videos")
@@ -134,7 +143,18 @@ class SHEET_FORM(form_sr.FORM):
     def select_srch_sites(self):
         st.write("Under construction - sites")
 
+    def write_srch_row_sum(self, row_sum):
+        rsum = str(row_sum).split(" ")
+        rsum.reverse()  # 2-digit sum will be at different rsum[idx] than 1 digit
+        if rsum[0] > "0":
+            st.write(":green[Found " + rsum[0] + " rows.]")
+        else:
+            st.write(":red[Found " + rsum[0] + " rows.]")
+        return rsum[0]
+
     dict_sql_df = {
         "web_pages": '''SELECT * FROM sheet_web_pages WHERE Description LIKE ('{}')''',
-        "web_pages_and_url": '''SELECT * FROM sheet_web_pages WHERE Description LIKE ('{}') OR URL LIKE ('{}')'''
+        "web_pages_count": '''SELECT COUNT(*) FROM sheet_web_pages WHERE Description LIKE ('{}')''',
+        "web_pages_and_url": '''SELECT * FROM sheet_web_pages WHERE Description LIKE ('{}') OR URL LIKE ('{}')''',
+        "web_pages_and_url_count": '''SELECT COUNT(*) FROM sheet_web_pages WHERE Description LIKE ('{}') OR URL LIKE ('{}')'''
     }
