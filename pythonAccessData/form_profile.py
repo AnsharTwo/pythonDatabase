42bbb6345403989dcac3462dc8c8fd0a889a9a95
AnsharTwo <robert.tomsett@btinternet.com>
2025-06-18 20:49:34 +0100

    profile help msg done

--- pythonAccessData/form_profile.py
+++ pythonAccessData/form_profile.py
@@ -12,7 +12,7 @@ class PROF_FORM (form_sr.FORM):
         self.authenticator = authenticator
 
     dict_data_locs = {
-        "librotate_close_msg": "NOTE: Please quit the background popup window after your have selected your file.",
+        "librotate_close_msg": "NOTE: Please quit the background popup window after you have selected your file.",
         "tkinter_close_msg": "PLEASE CLOSE THIS WINDOW AFTER YOU HAVE FINISHED SELECTING YOUR DATA SOURCE FILE."
     }
 
@@ -49,7 +49,6 @@ class PROF_FORM (form_sr.FORM):
         self.set_prof_flow_data_locs()
         if st.session_state.form_prof_flow == "profile settings - data locations":
             with st.form("data locations"):
-                #self.data_loc_msg()
                 config_data = self.load_ini_config()
                 st.markdown(f"**:blue[Data locations]**")
                 st.write("Annotations database")
@@ -62,17 +61,21 @@ class PROF_FORM (form_sr.FORM):
                         st.session_state.ss_dat_loc_annots).title() + ":orange[)]  ")
                     if st.form_submit_button("Browse annotations files"):
                         try:
-                            root = tk.Tk()
-                            tk.Button(root, text="Confirm data source file selection", font=("Arial", 12), command=root.destroy).pack()
-                            tk.Label(root, text = self.dict_data_locs.get("tkinter_close_msg")).pack()
-                            pth = self.ds_file_dialog("Access", ".mdb")
-                            if pth != "":
-                                config_data["data locations"]["annotations"] = pth
-                                st.session_state.ss_dat_loc_annots = pth
-                                self.write_ini_config(config_data)
-                            root.after(1000)
-                            root.mainloop()
-                            st.rerun()
+                            config_data = self.load_ini_config()
+                            if int(config_data.get('show_messages', 'profile_note')) == 1:
+                                self.__data_loc_modal()
+                            else:
+                                root = tk.Tk()
+                                tk.Button(root, text="Confirm data source file selection", font=("Arial", 12), command=root.destroy).pack()
+                                tk.Label(root, text = self.dict_data_locs.get("tkinter_close_msg")).pack()
+                                pth = self.ds_file_dialog("Access", ".mdb")
+                                if pth != "":
+                                    config_data["data locations"]["annotations"] = pth
+                                    st.session_state.ss_dat_loc_annots = pth
+                                    self.write_ini_config(config_data)
+                                root.after(1000)
+                                root.mainloop()
+                                st.rerun()
                         except Exception as ex:
                             st.write(":red[" + self.dict_err_msgs.get("tkinter_dialog_err") + "]")
                             st.markdown(ex)
@@ -83,18 +86,22 @@ class PROF_FORM (form_sr.FORM):
                     st.session_state.ss_dat_loc_urls).title() + ":orange[)]  ")
                 if st.form_submit_button("Browse URLs files"):
                     try:
-                        root = tk.Tk()
-                        tk.Button(root, text="Confirm data source file selection", font=("Arial", 12), command=root.destroy).pack()
-                        tk.Label(root, text = self.dict_data_locs.get("tkinter_close_msg")).pack()
-                        pth = self.ds_file_dialog("Excel", ".xlsx")
-                        if pth != "":
-                            config_data["data locations"]["urls"] = pth
-                            st.session_state.ss_dat_loc_urls = pth
-                            self.write_ini_config(config_data)
-                            self.load_book_sheet.clear() # clear cache or new data will not show
-                        root.after(1000)
-                        root.mainloop()
-                        st.rerun()
+                        config_data = self.load_ini_config()
+                        if int(config_data.get('show_messages', 'profile_note')) == 1:
+                            self.__data_loc_modal()
+                        else:
+                            root = tk.Tk()
+                            tk.Button(root, text="Confirm data source file selection", font=("Arial", 12), command=root.destroy).pack()
+                            tk.Label(root, text = self.dict_data_locs.get("tkinter_close_msg")).pack()
+                            pth = self.ds_file_dialog("Excel", ".xlsx")
+                            if pth != "":
+                                config_data["data locations"]["urls"] = pth
+                                st.session_state.ss_dat_loc_urls = pth
+                                self.write_ini_config(config_data)
+                                self.load_book_sheet.clear() # clear cache or new data will not show
+                            root.after(1000)
+                            root.mainloop()
+                            st.rerun()
                     except Exception as ex:
                         st.write(":red[" + self.dict_err_msgs.get("tkinter_dialog_err") + "]")
                         st.markdown(ex)
@@ -141,11 +148,17 @@ class PROF_FORM (form_sr.FORM):
                                 st.session_state.pwd_changed = True
                                 st.rerun()
 
-    @st.dialog("Before selecting your data source file", width="small")
-    def data_loc_msg(self):
+    @st.dialog("When selecting your data source file", width="small")
+    def __data_loc_modal(self):
         st.write(self.dict_data_locs.get("librotate_close_msg"))
-        #st.checkbox(label="Okay", key="lbrtt_dt_lc_swe")
-        #st.rerun()
+        cbx_data_loc_no_show = st.checkbox(label="Don't show this message again", key="lbrtt_dt_lc_swe")
+        if cbx_data_loc_no_show:
+            config_data = self.load_ini_config()
+            config_data["show_messages"]["profile_note"] = "0"
+            # if st.session_state.val_inpt_shw_drdg_msg:  # val of Settings > show messages > dredge web note
+            #     st.session_state.val_inpt_shw_drdg_msg = False
+            self.write_ini_config(config_data)
+            st.rerun()
 
     def ds_file_dialog(self, file_brand, file_type):
         input_path = fd.askopenfilename(title="Select a data location", filetypes=[(file_brand, file_type)])
